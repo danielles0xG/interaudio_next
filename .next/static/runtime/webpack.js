1e7ec17de4fe39dd7e3a187897c35a23fef165e7
Daniel-Ivan-Flores-Guerrero <daniel.ivan.flores.guerrero@ibm.com>
2019-08-06 19:04:24 -0400

    add admin view

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aa2d7239f70a618dce2e";
+/******/ 	var hotCurrentHash = "483eaeb875d254779638";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavDrawer from '../components/NavDrawer/NavDrawer';
+import Dashboard from '../admin/Dashboard'
+
+class Admin extends React.Component{
+	render(){
+		return( <Dashboard/> )
+	}
+}
+
+export default Admin;
