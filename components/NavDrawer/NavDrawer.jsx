e6e719f83a3c0cf37db282e6dd83022e1b98e3b6
Daniel-Ivan-Flores-Guerrero <daniel.ivan.flores.guerrero@ibm.com>
2019-08-06 20:27:20 -0400

    fix index

--- components/NavDrawer/NavDrawer.jsx
+++ components/NavDrawer/NavDrawer.jsx
@@ -28,7 +28,7 @@ function NavDrawer() {
 			onKeyDown={toggleDrawer(side, false)}>
 			<List className="nav_drawer__list">
 
-				<Link href='/'>
+				<Link href='/index'>
 					<ListItem button key='Inicio'>
 						<ListItemIcon>
 							<TiHomeOutline size='2rem' />
